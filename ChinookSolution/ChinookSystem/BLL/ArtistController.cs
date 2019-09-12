0d4dfecd81492799a1537906d4d2cedf2bfcc155
Don Welch <dwelch@nait.ca>
2019-09-11 19:39:00 -0600

    complete back lib app setup

---
+++ ChinookSolution/ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using ChinookSystem.DAL;
+using ChinookSystem.Data.Entities;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    public class ArtistController
+    {
+        public List<Artist> Artist_List()
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Artists.ToList();
+            }
+        }
+
+        public Artist Artist_Get(int artistid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Artists.Find(artistid);
+            }
+        }
+    }
+}
