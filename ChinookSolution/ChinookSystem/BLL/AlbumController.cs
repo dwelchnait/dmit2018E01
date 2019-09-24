1ffb818a8def4d9b7bcd3f8af4ce6a0c839c5a91
Don Welch <dwelch@nait.ca>
2019-09-24 10:37:55 -0600

    fix to MessageUserControl and ODS success message update

--- ChinookSolution/ChinookSystem/BLL/AlbumController.cs
+++ ChinookSolution/ChinookSystem/BLL/AlbumController.cs
@@ -60,6 +60,7 @@ namespace ChinookSystem.BLL
             {
                 if (CheckReleaseYear(item))
                 {
+                    item.Title = null;
                     context.Albums.Add(item);   //staging
                     context.SaveChanges();      //committed
                     return item.AlbumId;        //return new id value
